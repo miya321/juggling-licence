55008e4a3bdffa4d0c419c47f8e459d10b3ce95a
miya321 <miya.aziz@solirius.com>
2025-06-18 15:32:18 +0100

    not eligible page added

--- app/routes.js
+++ app/routes.js
@@ -18,3 +18,11 @@ router.post('/new/code', (req, res) => {
 router.post('/new/name', (req, res) => {
     res.redirect('/new/juggling-balls')
 })
+
+router.post('/new/juggling-balls', (req, res) => {
+    if(req.body.new.numberOfBalls == 'None, I cannot juggle') {
+        res.redirect('/new/not-eligible')
+    } else {
+        res.redirect('/new/juggling-trick')
+    }
+})
